78306c0d2c7c37eb6d37e1903414bb15b0202a68
Nahuel <31147468+Nacufala@users.noreply.github.com>
2018-04-19 21:06:36 -0300

    Comentarios de errores
    Corrijan lo que les pido en los comentarios
    
    Ayudante : Nahuel

--- blockChain.hs
+++ blockChain.hs
@@ -7,6 +7,10 @@ import Test.Hspec
 
 ------------TEST---------------------------------
 
+-- EN LOS TEST SE ESTA COMPARANDO USUARIOS CON UN NUMERO ( AL MARGEN DE QUE EL ENUNCIADO PEDIA DEVOLVER BILLETERAS Y DEVUELVEN USUARIOS ) Y NO SE PUEDE COMPARAR DOS TIPOS DIFERENTES 
+-- TAMBIEN HAY MUCHOS PARENTESIS DE MAS, RELEER O REPASAR LA PRESEDENCIA DE LAS FUNCIONES EN HASKELL
+-- Y ERRORES COMO ESTE "(upgrade.(extracion 2).(deposita 15 pepe)) `shouldBe` 27,6" DONDE HAY CLAROS ERRORES DE COMPOSICION 
+
 usuarioCon10Monedas = Usuario "Pepe" 10 10 
   
 
@@ -39,7 +43,9 @@ ejecutarTest = hspec $ do
     it "pepe le da 7 unidades a Lucho, deberia quedar lucho con 9 monedas" $ pepeLeDa7UnidadesALucho lucho lucho `shouldBe` nuevaBilletera 9 lucho
     it "pepe le da 7 unidades a Lucho y despues pepe deposita 5 monedas, deberia quedar con 9 monedas" $ (pepeLeDa7UnidadesALucho pepe.pepeDeposita5Monedas pepe) pepe `shouldBe` nuevaBilletera 8 pepe
 
------------TIPOS DE DATOS-----------------
+-----------TIPOS DE DATOS----------------- 
+
+-- NO SE ENTIENDE QUE FUNCION CUMPLE EL TYPE Transaccion SI ES IGUAL AL Evento Y DE TODAS MANERAS NO SE USA "Evento" EN EL CODIGO Y DEBERIA USARSE 
 
 type Evento = Usuario -> Usuario
 type Transaccion = Evento
@@ -51,7 +57,7 @@ type Nivel = Int
 data Usuario = Usuario {
   nombre :: Nombre,
   billetera :: Monedas,
-  nivel :: Nivel
+  nivel :: Nivel                                   -- NO SE NECESITA NINGUN NIVEL, RELEER BIEN EL ENUNCIADO
 } deriving (Show,Eq) 
 
 --------------USUARIOS-------------------
@@ -60,7 +66,10 @@ pepe = Usuario "Jose" 10 0
 lucho = Usuario "luciano" 2 0
 
 -----------EVENTOS-------------------------
-quedaIgual usuario = usuario
+-- ACA NUEVAMENTE HAY PARENTESIS DE MAS Y LA FUNCION subirNivel ES TOTALMENTE INNECESARIA 
+-- TIENEN QUE DELEGAR MAS EN LA FUNCION extraccion Y NO!! USEN GUARDA CUANDO DELEGUEN LA FUNCION (VIMOS UN EJEMPLO PARECIDO EN LAS PRIMERAS CLASES )
+
+quedaIgual usuario = usuario -- USAR LA FUNCION id
 nuevaBilletera unMonto unUsuario = unUsuario {billetera = unMonto}
 cierraCuenta usuario = nuevaBilletera 0 usuario
 subeNivel usuario = usuario {nivel = nivel usuario + 1 , billetera = (billetera usuario) * 1.2}
@@ -78,6 +87,10 @@ upgrade usuario
   | otherwise = nuevaBilletera (billetera usuario + 10) usuario
 
 ------------------TRANSACCIONES------------------
+
+-- EL ENUNCIADO PEDIA QUE EL CODIGO NO SE REPITA Y SEA FACILMENTE ESCALABLE, Y TIENEN MUCHO CODIGO REPETIDO
+-- CREEN UNA FUNCION PARA CREAR LAS TRANSACCIONES DE FORMA GENERICA Y LO MISMO CON LOS PAGOS 
+-- A MANERA DE AYUDA DEBERIAN PASARLE EL Evento Y LOS Usuarios Y DEVOLVERIAN UN Evento
 
 luchoCierraLaCuenta :: Usuario -> Transaccion
 luchoCierraLaCuenta unUsuario | compararUsuarios unUsuario lucho = cierraCuenta 
@@ -106,6 +119,8 @@ pepeLeDa7UnidadesALucho unUsuario | compararUsuarios unUsuario pepe = extraccion
 
 
 -----------------------BLOQUES----------------------------------
+
+-- LAS LISTAS NO TIENEN ESE FORMATO 
 
 bloque1 unUsuario =  (luchoCierraLaCuenta unUsuario
                       .pepeDeposita5Monedas unUsuario
